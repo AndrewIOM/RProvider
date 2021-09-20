d574eef382a59d66f2af93facbbe88d841cbfcb1
AndrewIOM <AndrewIOM@users.noreply.github.com>
2021-09-20 17:43:31 +0100

    Add more logs

--- src/RProvider.DesignTime/RProvider.fs
+++ src/RProvider.DesignTime/RProvider.fs
@@ -16,15 +16,19 @@ type public RProvider(cfg:TypeProviderConfig) as this =
     let useReflectionOnly = false//true
         
     let runtimeAssembly =
+        Logging.logf $"Looking for assembly..."
         if useReflectionOnly
         then
             let coreAssembly = typeof<obj>.Assembly
             let resolver = PathAssemblyResolver([ cfg.RuntimeAssembly; coreAssembly.Location ])
             use mlc = new MetadataLoadContext(resolver, coreAssemblyName = coreAssembly.GetName().Name)
             mlc.LoadFromAssemblyPath cfg.RuntimeAssembly
-        else Assembly.LoadFrom cfg.RuntimeAssembly
+        else 
+          Logging.logf "Loading assembly %s" cfg.RuntimeAssembly
+          Assembly.LoadFrom cfg.RuntimeAssembly
 
     static do 
+      Logging.logf $"Debug Constructor 2"
       // When RProvider is installed via NuGet, the RDotNet assembly and plugins
       // will appear typically in "../../*/lib/net40". To support this, we look at
       // RProvider.dll.config which has this pattern in custom key "ProbingLocations".
@@ -34,6 +38,7 @@ type public RProvider(cfg:TypeProviderConfig) as this =
       
     // Generate all the types and log potential errors
     let buildTypes () =
+        Logging.logf "Building types"
         try 
           for ns, types in RTypeBuilder.initAndGenerate(runtimeAssembly) do
             this.AddNamespace(ns, types)
