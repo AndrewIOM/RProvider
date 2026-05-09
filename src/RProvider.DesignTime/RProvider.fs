6d324f4c836fd06bf0c4763ad2aceef6829d89da
AndrewIOM <AndrewIOM@users.noreply.github.com>
2026-05-09 21:40:46 +0000

    Remove redundant Metadatacontext reference

--- src/RProvider.DesignTime/RProvider.fs
+++ src/RProvider.DesignTime/RProvider.fs
@@ -16,18 +16,7 @@ type public RProvider(cfg: TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces(cfg,
         assemblyReplacementMap = [ "RProvider.DesignTime", "RProvider" ])
 
-
-    let useReflectionOnly = false //true
-
-    let runtimeAssembly =
-        if useReflectionOnly then
-            let coreAssembly = typeof<obj>.Assembly
-            let resolver = PathAssemblyResolver([ cfg.RuntimeAssembly; coreAssembly.Location ])
-            use mlc = new MetadataLoadContext(resolver, coreAssemblyName = coreAssembly.GetName().Name)
-            LogFile.logf "[DesignTime] Loading runtime assembly %O" mlc
-            mlc.LoadFromAssemblyPath cfg.RuntimeAssembly
-        else
-            Assembly.LoadFrom cfg.RuntimeAssembly
+    let runtimeAssembly = Assembly.LoadFrom cfg.RuntimeAssembly
 
     // Generate all the types and log potential errors
     let buildTypes () =
